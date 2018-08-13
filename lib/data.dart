eb11c1673188327c42cdf55fc8e1e82579684ecf
SPGB <avonwodahs+github@gmail.com>
2018-08-13 16:11:25 -0400

    Added data descriptions

--- lib/data.dart
+++ lib/data.dart
@@ -1,16 +1,16 @@
 import 'video_content.dart';
 
 final List<VideoContent> data = <VideoContent>[
-  VideoContent("Mule",
-              "Sample video description body Text",
+  VideoContent("Big Buck Bunny",
+              "A large and lovable rabbit deals with three tiny bullies, led by a flying squirrel, who are determined to squelch his happiness.",
               "graphics/panda.png",
               "http://yt-dash-mse-test.commondatastorage.googleapis.com/media/car-20120827-87.mp4"),
-  VideoContent("Catatonic",
-              "Another sample video's description body text",
+  VideoContent("Octopus Carpenter: the Movie",
+              "To find the octopus carpenter is to discover underwater treasure. The animation has the depth of the ocean itself. The adventure is both fun and fulfilling. And the lessons just keep on swimming.",
               "graphics/panda.png",
               "http://yt-dash-mse-test.commondatastorage.googleapis.com/media/motion-20120802-87.mp4"),
-  VideoContent("Burlap",
-      "Another sample video's description body text",
+  VideoContent("Vusr the Musical",
+      "Vusr is a white-label publishing tool that allows creators and publishers to reach and monetize a large audience across all VR platforms",
       "graphics/panda.png",
       "http://yt-dash-mse-test.commondatastorage.googleapis.com/media/motion-20120802-87.mp4"),
 ];
