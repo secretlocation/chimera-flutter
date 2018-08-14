8aa008e960440b51ff2d2abfb1309b7e1896b534
TO-SL-VUSR00087\ChristianFerreira <cf3rreira@gmail.com>
2018-08-14 13:58:32 -0400

    moving forward with getting info area to show/hide

--- lib/content_card.dart
+++ lib/content_card.dart
@@ -1,10 +1,29 @@
 import 'package:chimera_flutter/video_content.dart';
 import 'package:flutter/material.dart';
+import 'package:chimera_flutter/info_button.dart';
 
-class ContentCard extends StatelessWidget {
+class ContentCard extends StatefulWidget {
   const ContentCard(this.data);
-
   final VideoContent data;
+
+  _ContentCard createState() => new _ContentCard();
+}
+
+class _ContentCard extends State<ContentCard>{
+
+  bool shouldShowDescription = false;
+  String descriptionText = "";
+
+  void showDescription(){
+    if(shouldShowDescription){
+      descriptionText = widget.data.description;
+    }
+    else{
+      descriptionText = "";
+    }
+    shouldShowDescription = !shouldShowDescription;
+    setState(() { });
+  }
 
   Widget build(BuildContext context) {
     return new Container(
@@ -33,15 +52,28 @@ class ContentCard extends StatelessWidget {
           ),
 
           new Positioned(
+            left: (MediaQuery.of(context).size.width * 0.5 - 25),
+            top: (MediaQuery.of(context).size.width * 0.75),
+            child:
+              new Text(descriptionText),
+          ),
+
+          new Positioned(
             left: (MediaQuery.of(context).size.width * 0.825),
             top: (MediaQuery.of(context).size.width * 0.6),
+
             child: new Column(
+
               children: <Widget>[
+                new Text(
+                 widget.data.runtime.toString()
+                ),
                 new IconButton(
                     iconSize: 50.0,
                     icon: const Icon(Icons.info),
-                    onPressed: () {}
+                    onPressed: () {showDescription();}
                 ),
+                //new InfoButton(),
                 new IconButton(
                     iconSize: 50.0,
                     icon: const Icon(Icons.settings),
