a7db89a649e10f0497fe612a09f356e345961083
Dylan Wallace <dwyllace@gmail.com>
2018-08-14 13:31:04 -0400

    Building actual video layout and callbacks

--- lib/video_view.dart
+++ lib/video_view.dart
@@ -1,2 +1,109 @@
+import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
+class VideoView extends StatefulWidget
+{
+  @override
+  _VideoViewState createState() => _VideoViewState();
+}
+
+class _VideoViewState extends State<VideoView> {
+
+  VideoPlayerController _controller;
+  bool _isPlaying = false;
+  VoidCallback playingListener;
+
+  // Initialize the playing listener
+  _VideoViewState(){
+    playingListener = () {
+      final bool isPlaying = _controller.value.isPlaying;
+      if (isPlaying != _isPlaying) {
+        setState(() {
+          _isPlaying = isPlaying;
+        });
+      }
+    };
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Create a video player with listeners to re-render on init and on play
+    _controller = VideoPlayerController.network(
+      // Example content sourced from https://github.com/SamsungInternet/examples
+      'https://github.com/SamsungInternet/examples/blob/master/360-video/paris-by-diego.mp4?raw=true',
+    )
+      ..addListener(playingListener)
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    _controller..removeListener(playingListener);
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return VideoPlayer(_controller);
+  }
+}
+
+/*
+class _VideoViewState extends State<VideoView> {
+  VideoPlayerController _controller;
+  bool _isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+      // Example content sourced from https://github.com/SamsungInternet/examples
+      'https://github.com/SamsungInternet/examples/blob/master/360-video/paris-by-diego.mp4?raw=true',
+    )
+      ..addListener(() {
+        final bool isPlaying = _controller.value.isPlaying;
+        if (isPlaying != _isPlaying) {
+          setState(() {
+            _isPlaying = isPlaying;
+          });
+        }
+      })
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video Demo',
+      home: Scaffold(
+        body: Center(
+          child: _controller.value.initialized
+              ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          )
+              : Container(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _controller.value.isPlaying
+              ? _controller.pause
+              : _controller.play,
+          child: Icon(
+            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+        ),
+      ),
+    );
+  }
+}
+*/
