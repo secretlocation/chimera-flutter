74b40faf22d03c7e959d755a8b818a777d4d85a4
Dylan Wallace <dwyllace@gmail.com>
2018-08-15 19:28:40 -0400

    Added play/pause

--- lib/video_view.dart
+++ lib/video_view.dart
@@ -16,19 +16,6 @@ class _VideoViewState extends State<VideoView> {
   bool _isLoaded = false;
   bool _isPlaying = false;
   bool _controlsVisible = true;
-  VoidCallback playingListener;
-
-  // Initialize the playing listener
-  _VideoViewState(){
-    playingListener = () {
-      final bool isPlaying = _controller.value.isPlaying;
-      if (isPlaying != _isPlaying) {
-        setState(() {
-          _isPlaying = isPlaying;
-        });
-      }
-    };
-  }
 
   void playAfterInit(_){
     _isLoaded = true;
@@ -53,7 +40,6 @@ class _VideoViewState extends State<VideoView> {
     _controller = VideoPlayerController.network(
       widget.currentVideo.videoUrl
     )
-      ..addListener(playingListener)
       ..setVolume(1.0);
 
     // Initialize the player with content, handling errors
@@ -70,7 +56,6 @@ class _VideoViewState extends State<VideoView> {
   void deactivate() {
     super.deactivate();
     _controller
-      ..removeListener(playingListener)
       ..setVolume(0.0);
   }
 
@@ -103,26 +88,76 @@ class VideoControls extends StatefulWidget
   VideoPlayerController controller;
   VideoContent content;
   bool isLoaded = false;
+
   VideoControls({Key key, @required this.controller, this.content, this.isLoaded}) : super(key: key);
   @override
   _VideoControlsState createState() => _VideoControlsState();
 }
 class _VideoControlsState extends State<VideoControls>
 {
+  bool _isPlaying = false;
+  VoidCallback playingListener;
+
+  // Initialize the playing listener
+  _VideoControlsState(){
+    playingListener = () {
+      final bool isPlaying = widget.controller.value.isPlaying;
+      if (isPlaying != _isPlaying) {
+        setState(() {
+          _isPlaying = isPlaying;
+        });
+      }
+    };
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller
+      ..addListener(playingListener);
+  }
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     var childs = <Widget>[];
 
     if(!widget.isLoaded) childs.add(LoadingSpinner(widget.content.customColor));
     else {
-
+      childs.add(
+        Positioned(
+          left: (width * 0.3),
+          top: (height * 0.5 - (width * 0.2)),
+          width: (width * 0.4),
+          height: (width * 0.4),
+          child: GestureDetector(
+            onTap: () {
+              if(_isPlaying){
+                widget.controller.pause();
+              }
+              else {
+                widget.controller.play();
+              }
+            },
+            child: PlayPauseButton(backgroundImage: widget.content.playButton, isPause: _isPlaying),
+          ),
+        )
+      );
+      childs.add(VideoControlsBottomBar(widget.controller, widget.content));
     }
-    childs.add(VideoControlsBottomBar(widget.controller, widget.content));
 
     return new Stack(
       children: childs,
     );
+  }
+
+  @override
+  void deactivate() {
+    widget.controller
+      ..removeListener(playingListener);
+    super.deactivate();
   }
 }
 
@@ -230,6 +265,23 @@ class LoadingSpinner extends StatelessWidget
       child: new CircularProgressIndicator(
         backgroundColor: mainColor,
       ),
+    );
+  }
+}
+
+class PlayPauseButton extends StatelessWidget
+{
+  AssetImage backgroundImage;
+  bool isPause;
+  PlayPauseButton({@required this.backgroundImage, @required this.isPause});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image(image: backgroundImage, fit: BoxFit.contain, gaplessPlayback: true),
+        Image(image: AssetImage( isPause ? "graphics/pause.png" : "graphics/play.png"), fit: BoxFit.contain, gaplessPlayback: true),
+      ],
     );
   }
 }
